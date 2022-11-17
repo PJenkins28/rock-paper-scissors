4b79997838b6f7a818f005ef556dfc44b45e0aa2
Paige Jenkins <paigemjenkins28@gmail.com>
2022-11-16 19:50:07 -0500

    created HTML skeleton and outlined js functions

---
+++ main.js
@@ -0,0 +1,23 @@
+function getComputerChoice() {
+
+}
+
+function getPlayerChoice() {
+
+}
+
+function playRound() {
+
+}
+
+function resetScores() {
+
+}
+
+function displayWinner() {
+
+}
+
+function game() {
+    
+}
