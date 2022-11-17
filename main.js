7809dde7da3e1968d61dd7e91de1bf12665b4eb4
Paige Jenkins <paigemjenkins28@gmail.com>
2022-11-16 19:54:18 -0500

    getComputerChoice() function

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let computerScore = 0;
 let playerScore = 0;
 
 function getComputerChoice() {
+const choiceArray = ['rock', 'paper', 'scissors'];
+let selection = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+return selection;
 
 }
 
